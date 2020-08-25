5e5e040cb4e442ceb0ea7411bcfcda1110591f16
ManuelRiano09 <mfriano95@misena.edu.co>
2020-08-25 18:49:43 -0500

    incio del proyecto I-FIT backend en java

---
+++ I-FIT-BACKEND/src/main/java/org/i_fit/IFitApplication.java
@@ -0,0 +1,13 @@
+package org.i_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IFitApplication.class, args);
+	}
+
+}
