fc35b7cffc69cd3b809f6499e95e3bc83c472021
ManuelRiano09 <mfriano95@misena.edu.co>
2020-08-27 03:15:00 -0500

    organizacion del codigo, listo para mergear con develop

--- I-FIT-BACKEND/src/main/java/org/i_fit/controller/MainController.java
+++ I-FIT-BACKEND/src/main/java/org/i_fit/controller/MainController.java
@@ -20,35 +20,5 @@ public class MainController {
     public List<Apprentice> getInfoForInstructor(){
         return apprenticeService.getInfoForInstructor();
     }
-//    @GetMapping("user")
-//    public String test2(){
-//
-//        Apprentice apprentice = new Apprentice();
-//        Routine routine = new Routine();
-//        Machine machine = new Machine();
-//
-//        machine.setName("Caminadora");
-//        machine.setDescription("sirve para caminar");
-//
-//        routine.setExercise("caminadora");
-//        routine.setRepetition(0);
-//        routine.setSeries(1);
-//        routine.setStatus(Status.StatusEnum.FINISHED);
-//        routine.setMachine(machine);
-//
-//        apprentice.setDocument(1001310159);
-//        apprentice.setEmail("manuel@hotmail");
-//        apprentice.setPassword("1234");
-//        apprentice.setName("Manuel Riaño");
-//        apprentice.setJourneyEnum(Journey.JourneyEnum.MORNING);
-//        apprentice.setWeight(56);
-//        apprentice.setAge(18);
-//        apprentice.setHeight(1.70);
-//        apprentice.setComments("quiero subir mi masa corporal");
-//        apprentice.setLimitations("ninguna");
-//        apprentice.setRoutine(routine);
-//
-//        apprenticeReposiroty.save(apprentice);
-//        return "ok";
-//    }
+
 }
