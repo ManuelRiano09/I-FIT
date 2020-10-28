4a1c98052d153a93b5b47fca01266e00eea5400c
ManuelRiano09 <mfriano95@misena.edu.co>
2020-10-27 20:28:44 -0500

    login de ingreso funcionando, falta el registro

--- I-FIT-BACKEND/src/main/java/org/i_fit/repositorybd/ApprenticeReposiroty.java
+++ I-FIT-BACKEND/src/main/java/org/i_fit/repositorybd/ApprenticeReposiroty.java
@@ -3,5 +3,5 @@ package org.i_fit.repositorybd;
 import org.i_fit.entities.responsesgym.Apprentice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ApprenticeReposiroty extends MongoRepository<Apprentice, Integer> {
+public interface ApprenticeReposiroty extends MongoRepository<Apprentice, String> {
 }
